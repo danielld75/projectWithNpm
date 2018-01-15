c8cd58519a83c37000c9739ffe89c801b25258b0
danielld75 <danielld@poczta.fm>
2018-01-15 20:18:15 +0100

    add emitter listener

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var OSInfo = require("./modules/OSInfo");
 var EventEmitter = require("events").EventEmitter;
 var emitter = new EventEmitter();
+emitter.on("beforeCommand", function(instruction){
+  console.log("You wrote: " + instruction + ", trying to run command");
+});
+emitter.on("afterCommand", function(){
+  console.log("Finished command")
+});
 
 process.stdin.setEncoding("utf-8");
 
